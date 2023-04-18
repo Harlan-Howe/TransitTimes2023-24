950a239a73d8ea2d0b5f0ee489c5bb62ea2f4418
Harlan Howe <hhowe7@gatech.edu>
2023-04-18 12:17:57 -0500

    Updated test4 to be more verbose, hopefully making the result of what I am asking for clearer.

--- MapConnectorTests.py
+++ MapConnectorTests.py
@@ -48,18 +48,31 @@ class MyTestCase(unittest.TestCase):
         # cv2.destroyAllWindows()
 
 
-    @unittest.skip("Skipping test 4. Get test 3 working first.")
+    # @unittest.skip("Skipping test 4. Get test 3 working first.")
     def test_4_west_east_path_description(self):
         edge_nums = [4,73,50,52,56,58,89,133,138,179,180]
+
         path = []
+        self.connector.first_city_id = 72
+        self.connector.second_city_id = 65
+        expected = "Path found:\n• Portland, OR\n• Boise, ID\n• Salt Lake City, UT\n• Cheyenne, WY\n• Lincoln, NE\n• Omaha, NE\n• Des Moines, IA\n• Chicago, IL\n• Toledo, OH\n• Cleveland, OH\n• Buffalo, NY\n• Syracuse, NY\ntotal_distance = 156894.0	total_time = 4640760.0"
+
+        print(f"You are starting at city #{self.connector.first_city_id}, which is {self.connector.vertices[self.connector.first_city_id]}.")
+        print("You're being given a path that includes the following: \n\tCity1\tCity2\tdist\ttime")
         for num in edge_nums:
             path.append((self.connector.edges[num]))
-        self.connector.first_city_id=72
-        self.connector.second_city_id= 65
+            print(f"\t{self.connector.edges[num]}")
+        print(f"You are ending at city #{self.connector.second_city_id}, which is {self.connector.vertices[self.connector.second_city_id]}.")
 
-        print(self.connector.describe_path(path))
-        self.assertEqual("Path found:\n• Portland, OR\n• Boise, ID\n• Salt Lake City, UT\n• Cheyenne, WY\n• Lincoln, NE\n• Omaha, NE\n• Des Moines, IA\n• Chicago, IL\n• Toledo, OH\n• Cleveland, OH\n• Buffalo, NY\n• Syracuse, NY\ntotal_distance = 156894.0	total_time = 4640760.0",
-                         self.connector.describe_path(path),
+        print("-------The computer is expecting the following description -----")
+        print(expected)
+        print("----------------------------------------------------------------")
+        print("Now you are building your own description.")
+        result = self.connector.describe_path(path)
+        print("------- You described this as ----------------------------------")
+        print(result)
+        print("----------------------------------------------------------------")
+        self.assertEqual(expected, result,
                          "West->East path did not match. You might have the correct cities, but different spacing/punctuation. Be sure to check the comparison.")
         # self.connector.display_path(path, line_color=(128, 128, 255))
         # cv2.waitKey()
