6dbef188b3c5000bd969d0343c9c20c91eb09bf1
Harlan Howe <hhowe7@gatech.edu>
2023-10-23 17:17:57 -0500

    Style fixes Added if __name__ == '__main__': code to make this a runner.

--- MapConnectorFile.py
+++ MapConnectorFile.py
@@ -368,3 +368,14 @@ class MapConnector:
         # ...but be sure to delete it for your finished program!
         # -----------------------------------------
         return result_path
+
+
+if __name__ == '__main__':
+    mp = MapConnector()
+    mp.start_process()
+
+    # traditionally, this will wait indefinitely until the user presses a key and
+    # then close the windows and quit. The loop in this program will make it so that
+    # it never really gets here, but it's a good habit.
+    cv2.waitKey()
+    cv2.destroyAllWindows()
